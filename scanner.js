3ece4b0bc81040e25809154ad0648883cfcea9b8
Tammam-Al-Bahri <tammam.al.bahri20@gmail.com>
2025-10-25 05:40:45 +0100

    addLootboxWeaponIds function (slow but works)

--- scanner.js
+++ scanner.js
@@ -5,7 +5,7 @@ async function getLootboxWeapons(mintAddress) {
         body: JSON.stringify({ mint: mintAddress }),
     });
     const data = await response.json();
-    console.log(data);
+    return data;
 }
 
 async function getWeaponData(weaponId) {
@@ -16,5 +16,46 @@ async function getWeaponData(weaponId) {
         }
     );
     const data = await response.json();
-    console.log(data);
+    return data;
 }
+
+async function addLootboxWeaponIds(lootboxes) {
+    const result = [];
+    let i = 1;
+    for (const box of lootboxes) {
+        try {
+            const data = await getLootboxWeapons(box.mint);
+            const weapons = data.weapons.map((w) => ({ id: w[1] }));
+
+            console.log(`${i} Adding weapon IDs to ${box.mint}`);
+            result.push({
+                lootbox: box.mint,
+                weapons,
+            });
+            i++;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return result;
+}
+
+const lootboxes = [
+    { mint: "MINT ADDRESS 1" },
+    { mint: "MINT ADDRESS 2" },
+    { mint: "MINT ADDRESS 3" },
+    { mint: "..." },
+];
+
+const data = await addLootboxWeaponIds(lootboxes);
+
+const blob = new Blob([JSON.stringify(data, null, 2)], { type: "application/json" });
+const url = URL.createObjectURL(blob);
+
+const a = document.createElement("a");
+a.href = url;
+a.download = "lootboxes-with-weapons.json";
+a.click();
+
+URL.revokeObjectURL(url);
